4c20bb6fccaca8a46a34a4f002878bbd27e4b4fb
anyafries <anya.fries@gmail.com>
2022-06-10 11:08:04 -0400

    Updated relapse data

--- R/data.R
+++ R/data.R
@@ -1,16 +1,18 @@
 #' Time-to-event data of cancer relapse
 #'
-#' A dataset containing the time-to-event of cancer relapse with 2 competing risks (labelled 2 and 3) in wide-form with patient information.
+#' A dataset containing the time-to-event of cancer relapse (event=1) with the competing risk death (event=2) in long-form with patient information.
 #'
-#' @format A data frame with 3649 rows and 8 columns:
+#' @format A data frame with 989 rows and 9 columns:
 #' \describe{
+#'   \item{ID}{Patient ID}
 #'   \item{Time}{Time-to-event}
 #'   \item{event}{Event of interest (0=censoring, 1=relapse, 2,3=competing risks)}
-#'   \item{ID}{Patient ID}
-#'   \item{age}{Patient's age at time of diagnosis}
-#'   \item{marker1, marker2, marker3}{Three patient biomarkers measured at baseline that do not change over time}
-#'   \item{treatment}{Indication for if the patient goes off treatment, in wide-form.
-#'   The entry is a time: before the time indicated, treatment=0 (on treatment) and afterwards is 1 (patient has gone off treatment)}
+#'   \item{age.at.time.0}{Patient's age at time of diagnosis}
+#'   \item{male}{Sex of patient, 1=male, 0=female}
+#'   \item{stage}{Cancer stage at diagnosis}
+#'   \item{bmi}{Patient's body mass index at diagnosis}
+#'   \item{treatment}{Patient's treatment status, treatment=1=on treatment, treament=0=patient is off treatment}
+#'   \item{fup_time}{Follow-up time, i.e., time at which this patient entry was created.}
 #' }
 #' @source \url{TODO}
 "relapse"
