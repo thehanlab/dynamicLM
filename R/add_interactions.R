2b1e752981551718a869f13242e317dbf9e5538e
anyafries <56112927+anyafries@users.noreply.github.com>
2023-02-23 19:23:40 -0800

    Rename LMdata to lmdata

--- R/add_interactions.R
+++ R/add_interactions.R
@@ -1,26 +1,39 @@
 # -----------------------------------------------------------------------
 #' Add landmarking time interactions to a super dataset
 #'
-#' @param LMdata An object of class "LMdataframe".
-#'   This can be created by running stack_data, or creating a stacked data set and storing it in a list with attributes outcome, w and end_time
-#'   (see stack_data for further description of outcome and w), end_time is the largest landmarking time.
-#' @param LMcovars Vector of strings indicating the columns that are to have a LM interaction
-#' @param func_covars A list of functions to use for interactions between LMs and covariates.
-#' @param func_LMs A list of functions to use for transformations of the landmark times.
-#' @param LM_col Character string specifying the column name that indicates the landmark time point for a row.
-#' @param keep Boolean value to indicate whether or not to keep the columns given by LMcovars without the time interactions or not. Default=FALSE.
+#' @param lmdata An object of class "LMdataframe".
+#'   This can be created by running [dynamicLM::stack_data()], or creating a
+#'   stacked data set and storing it in a list with attributes outcome, w and
+#'   end_time (see [dynamicLM::stack_data()] for further description of outcome
+#'   and w), end_time is the largest landmarking time.
+#' @param LMcovars Vector of strings indicating the columns that are to have a
+#'   LM interaction
+#' @param func_covars A list of functions to use for interactions between LMs
+#'   and covariates.
+#' @param func_LMs A list of functions to use for transformations of the
+#'   landmark times.
+#' @param LM_col Character string specifying the column name that indicates the
+#'   landmark time point for a row.
+#' @param keep Boolean value to indicate whether or not to keep the columns
+#'   given by LMcovars without the time interactions or not. Default=FALSE.
 #'
-#' @return An object of class "LMdataframe" which now also contains LM time-interactions.
+#' @return An object of class "LMdataframe" which now also contains LM
+#'   time-interactions.
 #'   The object has the following components:
-#'   - w, outcome: as the input (obtained from LMdata)
+#'   - w, outcome: as the input (obtained from lmdata)
 #'   - func_covars: as the input
 #'   - func_LMs: as the input
 #'   - LMcovars: as the input
 #'   - allLMcovars: a list of the new columns added
 #'   - LM_col: as the input
 #'
-#' @details For each variable "var" in LMcovars, new columns var_1,...,var_i (length(func_covars) == i) are added; one column for each interaction given in func_covars is added
-#'   Transformations of the LM column are added and labelled as LM_1,...,LM_j (length(func_LMs) == j); one column for each interaction given in func_LMs is added
+#' @details For each variable "var" in `LMcovars`, new columns var_1,...,var_i
+#'   (length(func_covars) == i) are added; one column for each interaction given
+#'   in func_covars is added
+#'
+#'   Transformations of the LM column are added and labelled as LM_1,...,LM_j
+#'   (length(func_LMs) == j); one column for each interaction given in func_LMs
+#'   is added
 #'
 #' @examples
 #' \dontrun{
@@ -32,24 +45,24 @@
 #' # Covariate-landmark time interactions
 #' func.covars <- list( function(t) t, function(t) t^2)
 #' # let hazard depend on landmark time
-#' func.LMs <- list( function(t) t, function(t) t^2)
+#' func.lms <- list( function(t) t, function(t) t^2)
 #' # Choose covariates that will have time interaction
 #' pred.covars <- c("age","male","stage","bmi","treatment")
 #' # Stack landmark datasets
-#' LMdata <- stack_data(relapse, outcome, LMs, w, covs, format="long",
-#'                      id="ID", rtime="fup_time", right=F)
+#' lmdata <- stack_data(relapse, outcome, LMs, w, covs, format="long",
+#'                      id="ID", rtime="T_txgiven", right=F)
 #' # Update complex LM-varying covariates, note age is in years and LM is in months
-#' LMdata$data$age <- LMdata$data$age.at.time.0 + LMdata$data$LM/12
+#' lmdata$data$age <- lmdata$data$age.at.time.0 + lmdata$data$LM/12
 #' # Add LM-time interactions
-#' LMdata <- addLMtime(LMdata, pred.covars, func.covars, func.LMs)
+#' lmdata <- add_interactions(lmdata, pred.covars, func.covars, func.lms)
 #' }
 #'
 #' @export
-addLMtime <- function(LMdata, LMcovars, func_covars, func_LMs, LM_col="LM",keep=T){
+add_interactions <- function(lmdata, LMcovars, func_covars, func_LMs, LM_col="LM",keep=T){
   if (LM_col %in% func_covars){
     stop(paste0("arg LM_col (given as ",LM_col,") should not be in arg func_covars."))
   }
-  data <- LMdata$data
+  data <- lmdata$data
   if (missing(func_covars)){
     # f gives covariate-time interactions
     f1 <- function(t) t
@@ -86,15 +99,15 @@ addLMtime <- function(LMdata, LMcovars, func_covars, func_LMs, LM_col="LM",keep=
     remaining = colnames(data)[! colnames(data)  %in% LMcovars]
     data <- data[remaining]
   }
-  LMdata$data <- data
+  lmdata$data <- data
 
-  LMdata$func_covars <- func_covars
-  LMdata$func_LMs <- func_LMs
-  LMdata$LMcovars <- LMcovars
-  LMdata$allLMcovars <- unique(allLMcovars)
-  LMdata$LM_col <- LM_col
+  lmdata$func_covars <- func_covars
+  lmdata$func_LMs <- func_LMs
+  lmdata$LMcovars <- LMcovars
+  lmdata$allLMcovars <- unique(allLMcovars)
+  lmdata$LM_col <- LM_col
 
-  return(LMdata)
+  return(lmdata)
 }
 
 
