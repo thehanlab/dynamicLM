4d81b8c5b5b896d71d9df8d4b7448616a6cb9772
anyafries <56112927+anyafries@users.noreply.github.com>
2023-03-20 23:08:40 -0700

    Add coef function

---
+++ R/coef.R
@@ -0,0 +1,10 @@
+#' Get the coefficients of a fitted supermodel in dynamicLM
+#'
+#' @param object
+#'
+#' @return Vector of coefficients for a Cox landmark supermodel or list of
+#'   coefficients for each cause-specific model for a CSC landmark supermodel.
+#' @export
+coef.dynamicLM <- function(object) {
+  return(coef(object$model))
+}
