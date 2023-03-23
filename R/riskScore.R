d89360fb9abf3156c65aab969781e7e3b4ec6521
anyafries <56112927+anyafries@users.noreply.github.com>
2023-03-22 17:33:35 -0700

    Guard against null LM_covars

--- R/riskScore.R
+++ R/riskScore.R
@@ -18,15 +18,22 @@ riskScore <- function(object, tLM, data, func_covars, func_lms)
   LM_covars <- pred_covars[idx_LM_covars]
   bet_covars <- pred_covars[-idx_LM_covars]
 
-  # coef_LM1*g1(t) + coef_LM2*g2(t) + ...
-  risk <- sum(
-    sapply(LM_covars, function(coef_name){
-      # Get associated function
-      idx <-  as.numeric(sub(".*\\D+", "\\1", coef_name))
-      return(func_lms[[idx]](tLM) * coefs[coef_name])
-    })
-    # X1*coef + X1*t*coef + X1*t^2*coef + ..
-  ) + sum(
+  if (!is.null(LM_covars)) {
+    # coef_LM1*g1(t) + coef_LM2*g2(t) + ...
+    risk1 <- sum(
+      sapply(LM_covars, function(coef_name){
+        # Get associated function
+        idx <-  as.numeric(sub(".*\\D+", "\\1", coef_name))
+        return(func_lms[[idx]](tLM) * coefs[coef_name])
+      })
+      # X1*coef + X1*t*coef + X1*t^2*coef + ..
+    )
+  } else {
+    risk1 <- 0
+  }
+
+  # X1*coef + X1*t*coef + X1*t^2*coef + ..
+  risk <- risk1 + sum(
     sapply(bet_covars, function(coef_name){
       # Get associated covariate info (remove _i from the name)
       covar <- sub("_\\d+$", "", coef_name)
