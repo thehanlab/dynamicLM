09271dd3c6eb287d793ab7cd5d1bb35a9cc3d71a
anyafries <56112927+anyafries@users.noreply.github.com>
2023-05-19 14:32:37 -0700

    Add functionality for interactions to be characters

--- R/add_interactions.R
+++ R/add_interactions.R
@@ -12,12 +12,17 @@
 #'
 #' @param lm_covs Vector of strings indicating the columns (covariates) that are
 #'   to have an interaction with the landmark times.
-#' @param func_covars A list of functions to use for interactions between
-#'   landmarks and covariates. For example,
-#'   `list( function(t) t, function(t) t^2)` will, for each covariate `x`,
-#'   create `x`, `x*t`, `x*t^2`.
-#' @param func_lms A list of functions to use for transformations of the
-#'   landmark times.
+#' @param func_covars Either a string (or vector of strings) specifying which
+#'   covariate(`x`)-landmark(`t`) interactions to include. One or
+#'   multiple of "linear" (`x, x*t`), "quadratic" (`x, x*t, x*t^2`),
+#'   "log" (`x, log(1 + x)`), or or "exp" (`x, exp(x)`).
+#'
+#'   Otherwise, a custom list of functions can be specified. For example,
+#'   `list( function(t) t, function(t) exp(20*t))` will, for each covariate `x`,
+#'   create `x, x*t, exp(20*t)`.
+#' @param func_lms Similar to `func_covars`: A list of functions to use for
+#'   transformations of the landmark times. Either a string or vector of
+#'   strings or a custom list of functions.
 #' @param lm_col Character string specifying the column name that indicates the
 #'   landmark time point for a row. Obtained from `lmdata` if not input.
 #' @param keep Boolean value to indicate whether or not to keep the columns
@@ -46,16 +51,12 @@
 #' \dontrun{
 #' data(relapse)
 #' outcome <- list(time = "Time", status = "event")
-#' covars <- list(fixed = c("ID","age.at.time.0","male","stage","bmi"),
+#' covars <- list(fixed = c("age.at.time.0", "male", "stage", "bmi"),
 #'                varying = c("treatment"))
 #' w <- 60; lms <- c(0, 6, 12, 18)
-#' LMs = seq(0,36,by=6)
-#' # Covariate-landmark time interactions
-#' func_covars <- list(function(t) t, function(t) t^2)
-#' # let hazard depend on landmark time
-#' func_lms <- list(function(t) t, function(t) t^2)
+#' LMs = seq(0, 36, by = 6)
 #' # Choose covariates that will have time interaction
-#' pred_covars <- c("age","male","stage","bmi","treatment")
+#' pred_covars <- c("age", "male", "stage", "bmi", "treatment")
 #' # Stack landmark datasets
 #' lmdata <- stack_data(relapse, outcome, lms, w, covars, format = "long",
 #'                      id = "ID", rtime = "T_txgiven")
@@ -63,7 +64,8 @@
 #' # note age is in years and LM is in months
 #' lmdata$data$age <- lmdata$data$age.at.time.0 + lmdata$data$LM/12
 #' # Add LM-time interactions
-#' lmdata <- add_interactions(lmdata, pred_covars, func_covars, func_lms)
+#' lmdata <- add_interactions(lmdata, pred_covars, func_covars = "quadratic",
+#'                            func_lms = "quadratic")
 #' head(lmdata$data)
 #' }
 #'
@@ -78,17 +80,29 @@ add_interactions <- function(lmdata, lm_covs, func_covars, func_lms, lm_col,
                 ") should not be in arg func_covars."))
   }
   data <- lmdata$data
-  if (missing(func_covars)){
-    # f gives covariate-time interactions
-    f1 <- function(t) t
-    f2 <- function(t) t^2
-    func_covars <- list(f1,f2)
+  f1 <- function(t) t
+  f2 <- function(t) t^2
+  f3 <- function(t) log(1 + t)
+  f4 <- function(t) exp(t)
+  if (missing(func_covars)) func_covars <- list(f1, f2)
+  if (missing(func_lms)) func_lms <- list(f1, f2)
+  if (inherits(func_covars, "character")){
+    funcs <- list()
+    if ("linear" %in% func_covars && !("quadratic" %in% func_covars))
+      funcs <- c(funcs, list(f1))
+    if ("quadratic" %in% func_covars) funcs <- c(funcs, list(f1, f2))
+    if ("log" %in% func_covars) funcs <- c(funcs, list(f3))
+    if ("exp" %in% func_covars) funcs <- c(funcs, list(f4))
+    func_covars <- funcs
   }
-  if (missing(func_lms)){
-    # g lets the hazard depend on time
-    g1 <- function(t) f1(t)
-    g2 <- function(t) f2(t)
-    func_lms <- list(g1,g2)
+  if (inherits(func_lms, "character")){
+    funcs <- list()
+    if ("linear" %in% func_lms && !("quadratic" %in% func_lms))
+      funcs <- c(funcs, list(f1))
+    if ("quadratic" %in% func_lms) funcs <- c(funcs, list(f1, f2))
+    if ("log" %in% func_lms) funcs <- c(funcs, list(f3))
+    if ("exp" %in% func_lms) funcs <- c(funcs, list(f4))
+    func_lms <- funcs
   }
 
   all_covs <- c(lm_covs)
