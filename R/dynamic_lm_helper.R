6c77609c5e486238d33966a087549c0f184637fe
anyafries <56112927+anyafries@users.noreply.github.com>
2024-02-05 20:39:06 +0200

    Add error checks for coxph vs CSC (formula, data, type)

--- R/dynamic_lm_helper.R
+++ R/dynamic_lm_helper.R
@@ -3,13 +3,37 @@ dynamic_lm_helper <- function(formula, type, data, lmdata, method, cluster,
                               all_covs, outcome, lm_col, original.landmarks,
                               args, ...) {
   # extra LHS of formula
-  LHS = getLHS(formula)
+  LHS <- getLHS(formula)
+
+  # check if type and formula match
+  if ((type == "coxph" && formula[[2]][[1]] != "Surv") ||
+      (type == "CSC" && formula[[2]][[1]] != "Hist")) {
+      stop(tidymess("Mismatch between formula's left-hand side (LHS) and type
+          of supermodel. For standard survival data (one event and possible
+          censoring, use type = \"coxph\" and a LHS of the form Surv(LM, Time,
+          event). For competing risks (multiple events and possible censoring),
+          use type = \"CSC\" and a LHS of the form Hist(Time, event, LM)"))
+  }
+
+  # check if the number of events matches too
+  # TODO: do this more rigorously, this assumes censoring is coded with a 0
+  if ((type == "coxph" && length(setdiff(unique(data[[outcome$status]]), 0)) > 1) ||
+      (type == "CSC" && length(setdiff(unique(data[[outcome$status]]), 0)) < 2)) {
+    stop(tidymess("Mismatch between the number of events and the type of model fit. For
+          standard survival data (one event and possible
+          censoring, use type = \"coxph\" and a LHS of the form Surv(LM, Time,
+          event). For competing risks (multiple events and possible censoring),
+          use type = \"CSC\" and a LHS of the form Hist(Time, event, LM)"))
+  }
 
   # check if we have a cluster term and extract it
   cluster_check <- as.character(stats::as.formula(formula))[3]
   if (!grepl("cluster", cluster_check)) {
     if (missing(cluster)) {
-      stop("Did you forget to specify a cluster argument or add a '+ cluster(..)' term in your formula for your ID variable? No cluster argument was specified in the formula. Standard errors may be estimated incorrectly.")
+      stop(tidymess("Did you forget to specify a cluster argument or add a '+
+                    cluster(..)' term in your formula for your ID variable? No
+                    cluster argument was specified in the formula. Standard
+                    errors may be estimated incorrectly."))
     } else {
       id_col <- cluster
     }
@@ -20,6 +44,7 @@ dynamic_lm_helper <- function(formula, type, data, lmdata, method, cluster,
     id_col <- cluster
   }
 
+  # Fit the model
   lm_covs <- intersect(sub("_[^_]+$", "", all.vars(formula[[3]])), lm_covs)
   if (type == "coxph") {
     superfm <- survival::coxph(formula, data, method = method, ...)
@@ -39,6 +64,7 @@ dynamic_lm_helper <- function(formula, type, data, lmdata, method, cluster,
     cl <- "LMCSC"
   }
 
+  # Get linear predictors
   linear.predictors <-
     t(sapply(1:num_causes, function(c) {
       coefs <- models[[c]]$coefficients
