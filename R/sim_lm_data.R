6a0da0fbac4acbf73d905e8ce28b8576b95cd989
anyafries <56112927+anyafries@users.noreply.github.com>
2024-02-05 15:48:37 +0200

    Improve formatting

--- R/sim_lm_data.R
+++ R/sim_lm_data.R
@@ -1,13 +1,29 @@
 ## TODO: sim.survdata outputs only whole units: user must be aware of this
 ## TODO: add num.data.frames argument
-## TODO: roxygen
 ## TODO: imports (dplyr?, tidyr?, coxed...)
 ## TODO: importantly!!!!!! handle longitudinal covariates
 ##       -> we're not updating covariate values at any point here
 ## TODO: remove multipier arg
+## TODO: roxygen
 
-sim_lm_data <- function(supermodel, data, N, hazard.fun = NULL, replace = TRUE,
-                        censor = FALSE, multiplier = 1, ...){
+#' Title
+#'
+#' @param supermodel
+#' @param data
+#' @param n
+#' @param hazard.fun
+#' @param replace
+#' @param censor
+#' @param multiplier
+#' @param tmax
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+sim_lm_data <- function(supermodel, data, n, hazard.fun = NULL, replace = TRUE,
+                        censor = FALSE, multiplier = 1, tmax, ...) {
   # {{{ Get some variables from the supermodel & check for missing variables
   if (inherits(supermodel, "LMCSC")) {
     models <- supermodel$model$models
@@ -24,17 +40,18 @@ sim_lm_data <- function(supermodel, data, N, hazard.fun = NULL, replace = TRUE,
   func_covars <- supermodel$func_covars
   func_lms <- supermodel$func_lms
   num_causes <- length(models)
-  Tmax <- w + 1
+  if (missing(tmax)) tmax <- w + 1
 
   if (missing(data)) stop("Argument data must be specified.")
-  if (missing(N)) N <- nrow(data)
+  if (missing(n)) n <- nrow(data)
   # }}}
 
-  # {{{ Create a function to get the hazard function if using the hazard function
+  # {{{ Create a function to get the hazard function if needed
   #    of the fitted model
-  if (!is.null(hazard.fun)) {
+  # TODO: argument checks for hazard.fun if it is user-specified
+  if (inherits(hazard.fun, "logical")) if (hazard.fun == TRUE) {
     # instant hazards for each cause-specific Cox model
-    instHaz <- lapply(1:num_causes, function(i) {
+    inst_haz <- lapply(1:num_causes, function(i) {
       base_data <- 0 * models[[i]]$coefficients
       sf <- survival::survfit(models[[i]], newdata = base_data)
       sf <- data.frame(time = sf$time, surv = sf$surv, Haz = -log(sf$surv))
@@ -48,38 +65,41 @@ sim_lm_data <- function(supermodel, data, N, hazard.fun = NULL, replace = TRUE,
         #       don't overlap, so can just use the OG hazard function...
         #       doesn't really change much but is worth noting
 
-        # risk <- riskScore(models[[i]], s, base_data, func_covars, func_lms)
         haz <- sf$Haz[pred_window] #* exp(risk)
-        instHaz <- haz[2:n_times] - haz[1:n_times - 1]
+        ihaz <- haz[2:n_times] - haz[1:n_times - 1]
         times <- sf$time[pred_window][2:n_times]
-        return(list("iH" = instHaz, "time" = times))
+        return(list("iH" = ihaz, "time" = times))
       })
     })
 
     # function to return hazard function for a given cause and landmark
     hazard.fun <- lapply(1:num_causes, function(cause) {
       lapply(seq_along(lms), function(s) {
-        iH <- instHaz[[cause]][[s]]
+        ihaz <- inst_haz[[cause]][[s]]
         func <- function(t) {
           sapply(t, function(ti) {
-            idx <- (lms[s] + ti - 1 <= iH$time) & (iH$time <= lms[s] + ti) # add all in one unit before
-            sum(iH$iH[idx]) * multiplier
+            # add all in one unit before
+            idx <- (lms[s] + ti - 1 <= ihaz$time) & (ihaz$time <= lms[s] + ti)
+            sum(ihaz$iH[idx]) * multiplier
           })
         }
-        return (func)
+        return(func)
       })
     })
   }
   # }}}
 
   # {{{ Simulate data
-  newdata <- data[sample(1:nrow(data), N, replace = replace), ] # subset of original data
-  newdata[[time_col]] <- NA   # this is what we're simulating
-  newdata[[event_col]] <- NA  # this is what we're simulating
-  idx <- rep(T, N) # inds we need to still simulate survival times for
+  # subset of original data to simulate from
+  newdata <- data[sample(seq_along(nrow(data)), n, replace = replace), ]
+  # this is what we're simulating
+  newdata[[time_col]] <- NA
+  newdata[[event_col]] <- NA
+  idx <- rep(TRUE, n) # inds we need to still simulate survival times for
 
   i <- 1
-  while (i <= length(lms) & sum(idx) != 0) {
+  while (i <= length(lms) && sum(idx) != 0) {
+    print(paste("-->", sum(idx)))
     # get curr landmark and data to simulate from
     t <- lms[i]
     curr_data <- newdata[idx, ]
@@ -87,11 +107,11 @@ sim_lm_data <- function(supermodel, data, N, hazard.fun = NULL, replace = TRUE,
     # simulate times for each cause
     times <- matrix(
       sapply(1:num_causes, function(c) {
-        risk_scores_c <- sapply(1:nrow(curr_data), function(row) {
+        risk_scores_c <- sapply(seq_len(nrow(curr_data)), function(row) {
           riskScore(models[[c]], t, curr_data[row, ], func_covars, func_lms)
         })
         risk_scores_c <- data.frame(risk_scores_c)
-        coxed::sim.survdata(X = risk_scores_c, T = Tmax, beta = 1, censor = 0,
+        coxed::sim.survdata(X = risk_scores_c, T = tmax, beta = 1, censor = 0,
                             hazard.fun = hazard.fun[[c]][[i]], ...)$data$y
       }),
       ncol = num_causes
@@ -118,26 +138,31 @@ sim_lm_data <- function(supermodel, data, N, hazard.fun = NULL, replace = TRUE,
   # censor anyone left
   cutoff <- max(lms) + w
   idx_na <- is.na(newdata[[time_col]]) | is.na(newdata[[event_col]])
+  # print(newdata[idx_na,])
+  # print(cutoff)
   newdata[idx_na, time_col] <- cutoff
   newdata[idx_na, event_col] <- 0
+  # print(newdata[idx_na,])
   # }}}
 
   # {{{ Censoring
 
   # TODO: adjust when we have longitudinal data
   # TODO: add covariate dependent censoring (copy sim.survdata?)
-  # TODO: add uniform/exponential censoring too
+  # TODO: add uniform/exponential censoring too (pick one/both?)
   # TODO: then censor = c("unif", "ind", "dep") ? Or maybe for dep
   #       need to specify a formula
-
-  if (censor == TRUE) {
+  if (is.numeric(censor)) {
+    censor_time <- rexp(n, rate = censor)
+    # censor_time <- runif(n) * censor
+  } else if (censor == TRUE) {
       args <- list(formula = as.formula(paste0("Hist(", time_col, ", ",
                                                event_col, ") ~ 1")),
                    data = data, reverse = TRUE)
       fit <- do.call(prodlim::prodlim, args)
-      print("model fit")
-      print(str(fit))
-      censoring_times <- sapply(1:N, function(i) {
+      # print("model fit")
+      # print(str(fit))
+      censor_time <- sapply(1:n, function(i) {
         # TODO: finding  closest proba could be better
         idx_surv <- which.min(abs(fit$surv - runif(1)))
         idx_time <- fit$surv == fit$surv[idx_surv]
@@ -147,9 +172,11 @@ sim_lm_data <- function(supermodel, data, N, hazard.fun = NULL, replace = TRUE,
         else
           fit$time[idx_time]
       })
-      print(censoring_times)
-      newdata[[time_col]] <- pmin(newdata[[time_col]], censoring_times)
-      newdata[newdata[[time_col]] == censoring_times, event_col] <- 0
+  }
+  if (censor != FALSE) {
+    # print(censor_time)
+    newdata[[time_col]] <- pmin(newdata[[time_col]], censor_time)
+    newdata[newdata[[time_col]] == censor_time, event_col] <- 0
   }
   # }}}
 
